bef78904545ddb24b38ec1d423324adf108eea1e
Greg Reinacker <gregr@rassoc.com>
2025-04-08 15:46:46 -0600

    rework cors

--- api/web-page-reader.ts
+++ api/web-page-reader.ts
@@ -53,6 +53,19 @@ const fetchAndCleanContent = async (url) => {
 };
 
 export default async function handler(req, res) {
+
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  
+
   // Only allow GET requests
   if (req.method !== 'GET') {
     return res.status(405).json({
